be027d431382567769de393de69134035363236c
israaothman <israaayoub@hotmail.com>
2020-05-19 20:30:58 +0300

    add more test cases

--- __test__/notes.test.js
+++ __test__/notes.test.js
@@ -9,8 +9,23 @@ describe('Notes Module',()=>{
   it('test the note object if console appears',()=>{
     let resevedObj =  { action: 'add', payload: 'hello' };
     const note = new Notes(resevedObj);
-    note.add(resevedObj);
+    note.execute(resevedObj);
     expect(console.log).toHaveBeenCalled();
   });
+
+  it('test the note object if console appears',()=>{
+    let resevedObj = 'ERROR : please Enter either -a or --add to add a note  ';
+    const note = new Notes(resevedObj);
+    note.execute(resevedObj);
+    expect(console.log).toHaveBeenCalled();
+  });
+
+  it('test the note object if console appears',()=>{
+    let resevedObj = 'ERROR : please Enter a vaild message inside qoutation ';
+    const note = new Notes(resevedObj);
+    note.execute(resevedObj);
+    expect(console.log).toHaveBeenCalled();
+  });
+
 
 });
