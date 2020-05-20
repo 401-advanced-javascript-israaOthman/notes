c92489c1d27842c94f1278aa65a550ea0c20087a
israaothman <israaayoub@hotmail.com>
2020-05-20 05:19:35 +0300

    almostDone

--- __test__/notes.test.js
+++ __test__/notes.test.js
@@ -8,24 +8,46 @@ describe('Notes Module',()=>{
   
   it('test the note object if console appears',()=>{
     let resevedObj =  { note: { action: 'add', payload: 'hello' }}; 
-    const note = new Notes(resevedObj);
+    const note = new Notes();
     note.execute(resevedObj);
     expect(console.log).toHaveBeenCalled();
   });
 
   it('test the note object if console appears',()=>{
     let resevedObj = { note: 'ERROR : please Enter either -a or --add to add a note  '};
-    const note = new Notes(resevedObj);
+    const note = new Notes();
     note.execute(resevedObj);
     expect(console.log).toHaveBeenCalled();
   });
 
   it('test the note object if console appears',()=>{
     let resevedObj = { note: 'ERROR : please Enter a vaild message inside qoutation '};
-    const note = new Notes(resevedObj);
+    const note = new Notes();
     note.execute(resevedObj);
     expect(console.log).toHaveBeenCalled();
   });
+
+  it('if the excute for delete',()=>{
+    let resevedObj =  { note: { action: 'delete', id: '5h5421456sd4ds642' }}; 
+    const note = new Notes();
+    note.execute(resevedObj);
+    expect(console.log).toHaveBeenCalled();
+  });
+
+  it('if the excute for list for all',()=>{
+    let resevedObj =  { note: { action: 'list', cat: 'all' }}; 
+    const note = new Notes();
+    note.execute(resevedObj);
+    expect(console.log).toHaveBeenCalled();
+  });
+
+  it('if the excute for list for one categiry',()=>{
+    let resevedObj =  { note: { action: 'list', cat: 'school' }}; 
+    const note = new Notes();
+    note.execute(resevedObj);
+    expect(console.log).toHaveBeenCalled();
+  });
+
 
 
 });
--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 #!/usr/bin/env node
 'use strict';
- 
+
 const Input = require('./lib/input.js');
 const Notee = require('./lib/notes.js');
+const mongoose = require('mongoose');
 
-const txt = new Input();
 
-const note = new Notee(txt);
+
+const MONGOOSE_URI ='mongodb://localhost:27017/notes';
+
+
+mongoose.connect(MONGOOSE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
+async function activate() {
+  const txt = new Input();
+  const note = new Notee();
+  await note.execute(txt);
+  mongoose.disconnect();
+}
+  
+activate();
+
+
